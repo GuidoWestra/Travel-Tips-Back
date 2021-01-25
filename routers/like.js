e9680e2a9b46e7c62727295458952239d6ac6fac
Guido Westra <westra.guido@gmail.com>
2021-01-25 18:08:20 +0100

    Fixed All Endpoints!!!

--- routers/like.js
+++ routers/like.js
@@ -2,7 +2,48 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
+const Like = require("../models/").like;
 
 const router = new Router();
+
+router.get("/likes/:tipId", async (req, res, next) => {
+  try {
+    const tipId = req.params.tipId;
+    const result = await Like.findAll({ where: { tipId } });
+    if (result) return res.status(200).send({ message: "Likes fetched!", data: result });
+  } catch (e) {
+    return res.status(400).send({ message: "Something went wrong in /likes/tipId!" });
+  }
+});
+router.post("/likes/:tipId", authMiddleware, async (req, res, next) => {
+  try {
+    const tipId = req.params.tipId;
+    const { id } = req.user;
+    const like = await Like.findOne({ where: { userId: id, tipId: tipId } });
+    if (like) return res.status(400).send({ message: "User already liked this" });
+    const result = await Like.create({
+      userId: id,
+      tipId,
+    });
+    if (result) return res.status(200).send({ message: "Like!", data: result });
+  } catch (e) {
+    return res.status(400).send({ message: "Something went wrong inside POST likes" });
+  }
+});
+router.delete("/likes/:tipId", authMiddleware, async (req, res, next) => {
+  try {
+    const tipId = req.params.tipId;
+    const { id } = req.user;
+    const result = await Like.destroy({
+      where: {
+        userId: id,
+        tipId,
+      },
+    });
+    if (result) return res.status(200).send({ message: "Like destroyed!", data: result });
+  } catch (e) {
+    return res.status(400).send({ message: "Something went wrong inside delete likes" });
+  }
+});
 
 module.exports = router;
